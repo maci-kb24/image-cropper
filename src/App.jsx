aab4c8bf58553f04028dc5cffddfe0ddb307c5bd
macikb8@yahoo.com <macikb8@yahoo.com>
2024-02-17 21:15:21 +0100

    create imagecropper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
+import { useEffect, useState } from "react";
 import DragZone from "./components/DragZone";
-import useFileReader from "./hooks/UseFileReader";
+import ImageCropper from "./components/ImageCropper";
+import { useFileReader } from "./hooks/UseFileReader";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-
+  const [openCropper, setOpenCropper] = useState(false);
   const [{ result, error, file }, setFile] = useFileReader({
     method: "readAsDataURL",
     accept: ["png", "jpg", "jpeg"],
     maxFileSize: 5,
-});
+  });
+
+  useEffect(() => {
+    if (result && !error.type) setOpenCropper(true);
+    else setOpenCropper(false);
+  }, [result, error.type]);
 
   return (
     <>
       <div className="relative min-h-screen flex flex-col items-center justify-center p-2 bg-white-50 font-poppins">
       <DragZone setFile={setFile} />
       {(error.type === "fileType" || error.type === "fileSize") && <p className="text-red-500 font-medium">{error.msg}</p>}
+      <AnimatePresence>
+        {openCropper && (
+          <ImageCropper
+            imageSrc={result}
+            file={file}
+            setFile={setFile}
+            setOpenCropper={setOpenCropper}
+          />
+        )}
+      </AnimatePresence>
     </div>
     </>
   )
--- src/hooks/UseFileReader.jsx
+++ src/hooks/UseFileReader.jsx
@@ -1,26 +1,58 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
 
-const useFileReader = (file) => {
-  const [dataUrl, setDataUrl] = useState(null);
-  const [error, setError] = useState(null);
+const bytesToMB = (bytes) => {
+    if (bytes === 0) return 0;
 
-  useEffect(() => {
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = () => {
-        setDataUrl(reader.result);
-      };
-
-      reader.onerror = () => {
-        setError('Error reading file');
-      };
-
-      reader.readAsDataURL(file);
-    }
-  }, [file]);
-
-  return { dataUrl, error };
+    return Math.round((bytes / (1024 * 1024)) * 100) / 100;
 };
 
-export default useFileReader;
+export const useFileReader = (options) => {
+    const { method, accept, maxFileSize } = options;
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState({ type: null, msg: null });
+    const [result, setResult] = useState(null);
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        if (!file) return;
+
+        const fileType = file.name.split(".").pop().toLowerCase(),
+            fileSizeMB = bytesToMB(file.size);
+
+        if (accept && !accept.includes(fileType)) {
+            setError({ type: "fileType", msg: `Invalid file type: ${fileType}. Accepted types: ${accept.join(", ")}` });
+        }
+
+        if (maxFileSize && fileSizeMB > maxFileSize) {
+            setError({ type: "fileSize", msg: `File size exceeds the maximum limit of ${maxFileSize} MB.` });
+        }
+
+        const fr = new FileReader();
+
+        fr.onprogress = (event) => {
+            if (event.lengthComputable) {
+                const percentLoaded = (event.loaded / event.total) * 100;
+                setProgress(percentLoaded);
+            }
+        };
+
+        fr.onload = () => {
+            setResult(fr.result);
+            setProgress(100);
+        };
+
+        fr.onerror = (err) => {
+            setError({ type: "fileRead", msg: `Error reading file: ${file.name}. ${err.message}` });
+        };
+
+        fr[method](file);
+
+        return () => {
+            setError({ type: null, msg: null });
+            setResult(null);
+            setProgress(0);
+        };
+    }, [file, accept, maxFileSize, method]); // Include missing dependencies here
+
+    return [{ result, error, file, progress }, setFile];
+};
